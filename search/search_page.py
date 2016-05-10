41fa5f92aee0ee2522bd3e7bb6490636c9f82963
Wesley Herts <wherts@broadinstitute.org>
2016-05-10 13:07:19 -0400

    minor styling for search page

--- search/search_page.py
+++ search/search_page.py
@@ -10,17 +10,24 @@ class Search:
 		self.title = "Search"
 		self.items = {}
 		self.root = Frame(parent)
-		self.text = tk.Text(self.root, height=1, )
+		#configure parent for grid
+		self.root.columnconfigure(0, weight=3)
+		# self.root.columnconfigure(1, weight=2)
+		self.root.columnconfigure(2, weight=1)
+
+		self.text = tk.Text(self.root, height=1, width=50)
 		self.text.bind("<Key>", self.build_word)
 		self.text.bind("<BackSpace>", self.delete)
 		self.text.bind("<Return>", self.lookup)
-		self.text.pack()
+		# self.text.pack()
 		self.build_dict()
-		self.labels = []
+		#spacing
+		tk.Label(self.root, text="Search:", font=("Helvetica", 16), bg=Search.bg_color).grid(row=0, column=0)
+		self.text.grid(row=0, column=1, columnspan=2)
 
 	def build_dict(self):
 		#specific item
-		self.items["dorito"] = ["Doritos Nacho Cheese, $4.00, 5", "Doritos Cooler Ranch, $4.00, 5", "Doritos Pizza Flavored, $4.00, 5"]
+		self.items["dorito"] = ["Doritos Nacho Cheese,$4.00,Aisle 5", "Doritos Cooler Ranch,$4.00,Aisle 5", "Doritos Pizza Flavored,$4.00,Aisle 5"]
 		#search for brand
 		self.items["kellogg's"] = ["Lucky Charms, $2.89, Aisle 3", "Cinnamon Toast Crunch, $3.00, Aisle 3", "Cocoa Puffs, $2.70, Aisle 3", "Raisin Bran, $4.00, Aisle 3"]
 		#search for food type
@@ -41,15 +48,11 @@ class Search:
 
 	# @param: upc is an identifier number that matches a specific item in the database
 	def lookup(self, event):
-		print "here"
-		print "Searching:", self.curr_search
+		# self.labels = []
 		for k in self.items.keys():
 			if k.lower().startswith(self.curr_search):
-				for t in self.items[k]:
-					self.labels.append(tk.Label(self.root, text=t, font=("Helvetica", 16), anchor=tk.NW, bg=Search.bg_color, justify=tk.LEFT, pady=5))
-		self.add_info()
+				for i, t in enumerate(self.items[k]):
+					# split_t = t.split(",")
+					# for j in range(len(split_t)):
+					tk.Label(self.root, text=t, font=("Helvetica", 14), bg=Search.bg_color, pady=5).grid(row=i+1, column=1)
 		self.curr_search = ""
-
-	def add_info(self):
-		for label in self.labels:
-			label.pack()
